16a2386a6a531925345357e4cf1f23f9b8df4741
Vincentius Reynaldo <60722646+vincentreyy@users.noreply.github.com>
2026-02-22 21:02:18 +0100

    ICPPIJ-1001
    Add Event Info at Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import HeroPage from "../components/HomePage/HeroPage";
 import ComingSoonPage from "../components/HomePage/ComingSoonPage";
+import InformationPage from "../components/HomePage/InformationPage";
 
 export default function Home() {
   return (
-    <main className="w-full">
+    <main 
+      className="w-full"
+      style={{
+                background: "linear-gradient(50deg, rgba(234,242,224,1) 0%, rgba(246,247,237,1) 50%, rgba(234,242,224,1) 100%)",
+            }} 
+    >
       <HeroPage />
       <ComingSoonPage />
+      <InformationPage />
     </main>
   );
 }
--- src/components/HomePage/InformationPage.tsx
+++ src/components/HomePage/InformationPage.tsx
@@ -1,7 +1,49 @@
+import Image from "next/image";
+import callToAction from "../../assets/LandingPage/benih-chat-bubble.png";
+import calendarIcon from "../../assets/LandingPage/calendar.png"
+import locationIcon from "../../assets/LandingPage/locpin.png"
+
 function InformationPage() {
     return(
-        <section className="flex flex-col relative">
-
+        <section className="min-h-screen flex flex-col justify-center w-full">
+            <div className="grid grid-cols-4 grid-rows-3 grid-flow-row min-h-screen justify-center items-center p-[15vw] relative">
+                <div className="row-span-3 self-start w-full h-full overflow-visible place-items-stretch absolute">
+                        <Image
+                            className="grid-row-start 1 grid-row-end-4"
+                            src={callToAction}
+                            fill
+                            alt="See you at ICONISSA 2026"
+                            style={{
+                                width: '100%',
+                                objectFit: 'contain',
+                            }}
+                        />
+                </div>
+                <div className="col-start-[-3] row-start-[-2] col-span-2 flex-row items-center content-between whitespace-nowrap text-black text-[2em] font-semibold">
+                    <div className="flex inline-grid gap-[1vw]">
+                        <div className="flex inline-flex items-center gap-5">
+                            <div className="relative w-16 h-16">
+                                <Image
+                                    src={calendarIcon}
+                                    alt="Calendar Icon"
+                                    
+                                />
+                            </div>
+                            <p>Technische Universität München <br /> Munich, Jerman</p>
+                        </div>
+                        <div className="flex inline-flex items-center gap-5"> 
+                            <div className="relative w-16 h-16">
+                                <Image
+                                    src={locationIcon}
+                                    alt="Location Icon"
+                                    fill
+                                />
+                            </div>
+                            <p>27-29 Agustus 2026</p>
+                        </div>   
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
